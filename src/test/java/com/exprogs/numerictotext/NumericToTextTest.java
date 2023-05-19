5cf0944e1fb5e1504284156fbafe952077451af1
nifmynif <115931873+nifmynif@users.noreply.github.com>
2023-05-19 20:58:16 +0300

    test add

--- src/test/java/com/exprogs/numerictotext/NumericToTextTest.java
+++ src/test/java/com/exprogs/numerictotext/NumericToTextTest.java
@@ -9,8 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class NumericToTextTest {
 
     @Test
-    void getText() throws DataFormatException {
-        NumericToText numeric = new NumericToText("0");
-        assertEquals("ноль", numeric.getText());
+    void getTextException() {
+        try {
+            NumericToText numeric = new NumericToText("");
+            assertEquals("DataFormatException", numeric.getText());
+        } catch (DataFormatException e) {
+            assertEquals("введено не число", e.getMessage());
+        }
+    }
+    @Test
+    void getTextZero() throws DataFormatException {
+            NumericToText numeric = new NumericToText("0");
+            assertEquals("ноль", numeric.getText());
+    }
+    @Test
+    void getTextOne() throws DataFormatException {
+        NumericToText numeric = new NumericToText("1");
+        assertEquals("один", numeric.getText());
+    }
+    @Test
+    void getTextMinesOne() throws DataFormatException {
+        NumericToText numeric = new NumericToText("-1");
+        assertEquals("Минус один", numeric.getText());
+    }
+    @Test
+    void getTextMinesOneHausenOneHandedOne() throws DataFormatException {
+        NumericToText numeric = new NumericToText("1 101");
+        assertEquals("одна тысяча сто один", numeric.getText());
     }
 }
